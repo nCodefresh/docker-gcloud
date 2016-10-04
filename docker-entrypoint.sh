189fb9b84f040325e2c046600e3c4cb4ed43ecbf
nikolai <n.ivanov.p@gmail.com>
2016-10-04 14:04:52 +0300

    added keys

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 gcloud auth activate-service-account $GCLOUD_EMAIL_SERVICE_ACCOUNT --key-file /keys/$GCLOUD_KEY_FILE --project $GCLOUD_PROJECT
 #gcloud config set project $GCLOUD_PROJECT
 gcloud info
-
+gcloud docker push gcr.io/cf-ci-1027/yaml-node
 if [ "$1" = 'gcloud' ]; then
 
     exec gcloud "$@"
