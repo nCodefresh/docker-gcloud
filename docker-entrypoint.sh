6ad8007f3585a16514d727da6168e78b075bbd8f
nikolai <n.ivanov.p@gmail.com>
2016-10-04 13:55:00 +0300

    added keys

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-gcloud auth activate-service-account $GCLOUD_EMAIL_SERVICE_ACCOUNT --key-file /keys/$GCLOUD_KEY_FILE
-gcloud config set project $GCLOUD_PROJECT 
+gcloud auth activate-service-account $GCLOUD_EMAIL_SERVICE_ACCOUNT --key-file /keys/$GCLOUD_KEY_FILE --project $GCLOUD_PROJECT
+#gcloud config set project $GCLOUD_PROJECT
+gcloud info
 
 if [ "$1" = 'gcloud' ]; then
 
